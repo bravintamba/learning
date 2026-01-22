0af65f32f42bd52335a362a87e5b9af02f1d681d
bravintamba <bravintamba@gmail.com>
2026-01-22 14:14:38 +0300

    exercise arithematic operations

---
+++ thinkpython/chapter1/exercise1.2.py
@@ -0,0 +1,14 @@
+seconds = 42
+minutes = 42
+total_seconds = seconds + (minutes * 60)
+print(total_seconds)
+
+kilometers = 10
+miles = kilometers / 1.61
+print(miles)
+
+distance_kilometers_to_miles = 10 / 1.61
+time_seconds_to_minutes = 42 / 60
+time_minutes_to_hours = time_seconds_to_minutes + (42 / 3600)
+speed_miles_per_hour = distance_kilometers_to_miles / time_minutes_to_hours
+print(speed_miles_per_hour)
